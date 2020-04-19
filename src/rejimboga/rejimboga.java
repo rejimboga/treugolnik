27a985c11da178a8fce66ff4ec6b359bfb67d1fd
PussyDestrojer <pashabagan973@gmail.com>
2020-04-19 15:50:37 +0300

    123

--- src/rejimboga/rejimboga.java
+++ src/rejimboga/rejimboga.java
@@ -15,10 +15,13 @@ public class rejimboga {
         System.out.print("Третя сторона:");
         int c = sc.nextInt();
 
-        if(a + b < c || b + c < a || a + c < b){
-            System.out.println("Триткутник не існує");
-        } else if (a + b > c || b + c > a || a + c > b){
-            System.out.println("Трикутник існує");
-        }
+        if (a != 0 && b != 0 && c != 0) {
+
+            if (a + b < c || b + c < a || a + c < b) {
+                System.out.println("Триткутник не існує");
+            } else if (a + b > c || b + c > a || a + c > b) {
+                System.out.println("Трикутник існує");
+            }
+        } System.out.println("Error");
     }
 }
