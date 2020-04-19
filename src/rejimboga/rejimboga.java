0cd97e92544ab068d9ec016824d824be0e6e918c
PussyDestrojer <pashabagan973@gmail.com>
2020-04-19 15:35:45 +0300

    Initial commit

---
+++ src/rejimboga/rejimboga.java
@@ -0,0 +1,24 @@
+package rejimboga;
+
+import java.util.*;
+
+public class rejimboga {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Перша сторона:");
+        int a = sc.nextInt();
+
+        System.out.print("Дурга сторона:");
+        int b = sc.nextInt();
+
+        System.out.print("Третя сторона:");
+        int c = sc.nextInt();
+
+        if(a + b < c || b + c < a || a + c < b){
+            System.out.println("Триткутник не існує");
+        } else if (a + b > c || b + c > a || a + c > b){
+            System.out.println("Трикутник існує");
+        }
+    }
+}
